b9e03a9aff6e890dcc66542e751aa88a1c06bff1
charlyautomatiza <charlyautomatiza@gmail.com>
2023-11-29 19:35:06 -0300

    test: add assertions

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -6,13 +6,11 @@ test('Login as Admin - Logout', async ({ page }) => {
   await page.getByRole('link', { name: 'Admin panel' }).click();
   await page.getByTestId('username').click();
   await page.getByTestId('username').fill('admin');
-  await page.getByTestId('password').click();
+  await page.getByTestId('username').press('Tab');
   await page.getByTestId('password').fill('password');
   await page.getByTestId('submit').click();
-
-  await expect(page.getByRole("link", { name: "Logout" })).toHaveText("Logout");
-
-  await page.getByRole("link", { name: "Logout" }).click();
-  await expect(page.getByRole("link", { name: "Logout" })).not.toBeVisible();
-
+  await expect(page.getByRole('link', { name: 'Logout' })).toBeVisible();
+  await page.getByRole('link', { name: 'Logout' }).click();
+  await expect(page.getByTestId('login-header')).toContainText('Log into your account');
+  await expect(page.getByTestId('submit')).toBeVisible();
 });

3af9992d2ba90a7ead65d7915c3f426825c9e907
charlyautomatiza <charlyautomatiza@gmail.com>
2024-09-21 19:49:33 -0300

    test: :test_tube: nuevos ejemplos de posibles assertions

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,16 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-test('Login as Admin - Logout', async ({ page }) => {
+test('As an Admin User, Login and Logout', async ({ page }) => {
   await page.goto('https://automationintesting.online/');
   await page.getByRole('button', { name: 'Let me hack!' }).click();
   await page.getByRole('link', { name: 'Admin panel' }).click();
+  await expect(page.getByTestId('login-header')).toBeVisible();
   await page.getByTestId('username').click();
   await page.getByTestId('username').fill('admin');
-  await page.getByTestId('username').press('Tab');
+  await page.getByTestId('password').click();
   await page.getByTestId('password').fill('password');
   await page.getByTestId('submit').click();
-  await expect(page.getByRole('link', { name: 'Logout' })).toBeVisible();
+  await expect(page.getByRole('navigation')).toContainText('Logout');
   await page.getByRole('link', { name: 'Logout' }).click();
   await expect(page.getByTestId('login-header')).toContainText('Log into your account');
-  await expect(page.getByTestId('submit')).toBeVisible();
 });
